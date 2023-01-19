b145257d120bab4c4edff93573342bc2c397fa73
Brenno Oliveira <120618226+Brenno0liveira@users.noreply.github.com>
2023-01-19 04:14:09 -0300

    Add files via upload

---
+++ meuprimeirojava.java
@@ -0,0 +1,25 @@
+//salva como meuprimeiroprogramajava.java
+
+//nome da classe
+class MeiPrimeiroJava
+{
+ //modulo principal com a entrada pela linha de comando
+ public static void main (String entrada[])
+ {
+   //declaração de variáveis
+     int inteiro = 47;
+     char character = 'F';
+     double real = 1.85;
+     String frase = "Brenno";
+     boolean VF=true;
+     
+     if (VF == true)
+     {
+
+       System.out.println("Eu sou o " + frase + "tenho" + inteiro +
+anos e trenho " + real + "m de altura");
+     }
+
+      System.exit(0);
+ }
+}
